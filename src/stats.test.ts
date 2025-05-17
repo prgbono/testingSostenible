fe3ca99116204ca3b46e7cb78bcb64c5bdb7a10d
Paco Ríos <pacoriosgalan@gmail.com>
2025-05-17 17:23:54 +0200

    kata1-Construir una librería de tests

---
+++ src/stats.test.ts
@@ -0,0 +1,35 @@
+import {average, sum} from "./stats";
+import {describe, expect, it, test} from "./testLib";
+import * as statsAsync from "./statsAsync";
+
+describe('Stats should', () => {
+    it('calculates the sum of all elements of the array', () =>{
+        const result = sum([1,2,3]);
+        const expected = 6;
+        expect(expected).toBe(result)
+    })
+
+    it('calculates the average of all elements of the array', () =>{
+        const result = average([1,2,3]);
+        const expected = 2;
+        expect(expected).toBe(result)
+    })
+})
+
+describe('Stats Async should', () => {
+    it('calculates the sum of all elements of the array (async)', async () =>{
+        const result = await statsAsync.sum([1,2,3]);
+        const expected = 6;
+        expect(expected).toBe(result)
+    })
+
+    it('calculates the average of all elements of the array (async)', async () =>{
+        const result = await statsAsync.average([1,2,3]);
+        const expected = 2;
+        expect(expected).toBe(result)
+    })
+
+})
+
+
+
